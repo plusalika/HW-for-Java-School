a2c933f4d430762fd188b9af479e09e56e7e725c
plusalika <32395620+plusalika@users.noreply.github.com>
2018-07-22 10:19:56 +0300

    Update task_one.pas

--- task_one.pas
+++ task_one.pas
@@ -3,17 +3,17 @@ var number, count, prev_count, current:integer;
 begin
   count:=1;
   prev_count:=0;
-    for current:=1 to length(a)-1 do begin
-      if (a[current]=a[current+1]) then begin
-        count:=count + 1;
-      end  
-      else begin
-        if count>prev_count then begin
-          prev_count:=count;
-          number:=a[current];
-          end;
-        count:=1;
-      end;
-    end;      
+  for current:=1 to length(a)-1 do begin
+    if (a[current]=a[current+1]) then begin
+      count:=count + 1;
+    end  
+    else begin
+      if count>prev_count then begin
+        prev_count:=count;
+        number:=a[current];
+        end;
+      count:=1;
+    end;
+  end;      
   finding_largest_recurring_sequence:=number;
 end;
