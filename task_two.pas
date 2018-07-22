91da3c7c9319a5cc52371efacab2e81f475d9ea8
plusalika <32395620+plusalika@users.noreply.github.com>
2018-07-22 10:18:18 +0300

    Update task_two.pas

--- task_two.pas
+++ task_two.pas
@@ -4,32 +4,32 @@ var base_length:integer;
     gd,gm,limit,counter,counter_line:integer; 
     const_y,const_x,x,y:integer;
 begin
-writeln('Please enter the bases length: ');
-readln(base_length);
-gd:=detect;
-initgraph(gd,gm,'');
-if graphresult<0 then halt(1);
+  writeln('Please enter the bases length: ');
+  readln(base_length);
+  gd:=detect;
+  initgraph(gd,gm,'');
+  if graphresult<0 then halt(1);
 
-x:=getmaxx div 2 - (base_length+1 div 2)*5;
-const_x:=x;
-y:=getmaxy div 2 + (base_length+1 div 2)*4;
-const_y:=y;
-setrgbpalette(21,255,122,0);
-setcolor(21);
+  x:=getmaxx div 2 - (base_length+1 div 2)*5;
+  const_x:=x;
+  y:=getmaxy div 2 + (base_length+1 div 2)*4;
+  const_y:=y;
+  setrgbpalette(21,255,122,0);
+  setcolor(21);
 
-limit:=base_length;
-for counter_line:=1 to (base_length+1 div 2) do begin
-  if limit>0  then begin
-    for counter:=1 to limit do begin
-      outtextxy(x,y,'$');
-      x:=x+10;
+  limit:=base_length;
+  for counter_line:=1 to (base_length+1 div 2) do begin
+    if limit>0  then begin
+      for counter:=1 to limit do begin
+        outtextxy(x,y,'$');
+        x:=x+10;
+      end;
+      limit:=limit - 2;
     end;
-    limit:=limit - 2;
+    x:=const_x+10*counter_line;
+    y:=const_y-counter_line*15;
   end;
-  x:=const_x+10*counter_line;
-  y:=const_y-counter_line*15;
-end;
-repeat
-until (keypressed);
-closegraph;
+  repeat
+  until (keypressed);
+  closegraph;
 end.
